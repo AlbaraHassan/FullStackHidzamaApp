57c4a4319845196549c37f6fa273d201eec03ba7
Albara Hassan <braa3300@hotmail.com>
2022-08-06 19:04:31 +0200

    Handling errors

--- server/appointments/admin.py
+++ server/appointments/admin.py
@@ -1,15 +1,34 @@
 from django.contrib import admin
+
+from .validators import validate_date
 from .models import Patient, Appointment
+from django import forms
 # Register your models here.
+
 
 class PatientAdmin(admin.ModelAdmin):
     model = Patient
     search_fields = ["id", "name"]
 
+
+class AppointmentForm(forms.ModelForm):
+
+    
+    model = Appointment
+
+    def clean(self):
+        try:
+            validate_date(self.year, self.month, self.day,
+                          self.hour, self.minute)
+        except:
+            raise forms.ValidationError("Date is not valid!")
+
+
 class AppointmentAdmin(admin.ModelAdmin):
     model = Appointment
+    form = AppointmentForm
     search_fields = ["patient__name", "patient__id"]
+
 
 admin.site.register(Patient, PatientAdmin)
 admin.site.register(Appointment, AppointmentAdmin)
-
