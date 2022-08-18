bf8c05987ff43151c3e98e7d6c53b5a0cc2f66e2
Albara Hassan <braa3300@hotmail.com>
2022-08-18 15:13:28 +0200

    Added react

--- client/components/NavigationBar.js
+++
@@ -1,6 +0,0 @@
-import { Box } from "@chakra-ui/react";
-
-
-export default function Nav() {
-    return <Box borderRadius={"md"} p={10} m={10} bg={"yellow.300"} fontSize={50} justifyContent={"center"} display={"flex"}>Hidzama</Box>
-}
